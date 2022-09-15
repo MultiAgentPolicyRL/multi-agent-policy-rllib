a66bdf8bc19650848bdeb414e164fe850a8407e3
Ettore Saggiorato <84920411+sa1g@users.noreply.github.com>
2022-09-15 09:14:06 +0000

    Delete ai-economist/tutorials/rllib==2.0.0 directory

--- ai-economist/tutorials/rllib==2.0.0/test.py
+++
@@ -1,8 +0,0 @@
-from pkg_resources import get_distribution
-
-if get_distribution('ray[rllib]').version == '0.8.3':
-    print('0.8.3')
-elif get_distribution('ray[rllib]').version == '2.0.0':
-    print(get_distribution('ray[rllib]').version)
-else:
-    print('ray[rllib] not supprted')
