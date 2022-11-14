885e71bddb7d127acab527aad49f907b77a9e6ca
Andrea Bonomi <andrea.bonomi489@gmail.com>
2022-11-14 16:15:37 +0100

    Update

--- PPO/keras_model_base.py
+++ PPO/keras_model_base.py
@@ -38,7 +38,7 @@ def feed_model(obs, model):
     return tf.reshape(model([obs['world-map'], obs['flat']]), [-1])
 
 
-def get_model(conv_filters, filter_size):
+def get_policy_model(conv_filters, filter_size):
     """ Builds the model. Takes in input the parameters that were not specified in the paper. """
     cnn_in = k.Input(shape=(7, 11, 11))
     map_cnn = k.Conv2D(conv_filters[0], filter_size, activation='relu')(cnn_in)
@@ -53,13 +53,54 @@ def get_model(conv_filters, filter_size):
 
     lstm = k.LSTM(128)(mlp1)
     mlp2 = k.Dense(50)(lstm)
+    #output = k.Dense(1, activation='relu')(mlp2)
 
-    ### Output layer must return an integer
-    output = k.Dense(1, activation='relu')(mlp2)
-
-    model = Model(inputs=[cnn_in, info_input], outputs=output)
-    #model = Model(inputs=[cnn_in, info_input], outputs=mlp2)
-
-    #print(model.summary())
+    model = Model(inputs=[cnn_in, info_input], outputs=mlp2)
 
     return model
+
+def get_model(state_shape, action_dim, units=(400, 300, 100)):
+    state = k.Input(shape=state_shape)
+
+    # Value function (baseline)
+    # used to calculate advantage estimate
+    vf = k.Dense(units[0], name="Value_L0", activation="tanh")(state)
+    for index in range(1, len(units)):
+        vf = k.Dense(units[index], name="Value_L{}".format(
+            index), activation="tanh")(vf)
+
+    value_pred = k.Dense(1, name="Out_value")(vf)
+
+    # Our Policy
+    cnn_in = k.Input(shape=(7, 11, 11))
+    map_cnn = k.Conv2D(state_shape[0], action_dim, activation='relu')(cnn_in)
+    map_cnn = k.Conv2D(state_shape[1], action_dim, activation='relu')(map_cnn)
+    map_cnn = k.Flatten()(map_cnn)
+
+    info_input = k.Input(shape=(136))
+    mlp1 = k.Concatenate()([map_cnn, info_input])
+    mlp1 = k.Dense(128, activation='relu')(mlp1)
+    mlp1 = k.Dense(128, activation='relu')(mlp1)
+    mlp1 = k.Reshape([1, -1])(mlp1)
+
+    lstm = k.LSTM(128)(mlp1)
+    mlp2 = k.Dense(action_dim, activation="softmax")(lstm)
+
+    #pi = k.Dense(units[0], name="Policy_L0", activation="tanh")(state)
+    #for index in range(1, len(units)):
+    #    pi = k.Dense(units[index], name="Policy_L{}".format(
+    #        index), activation="tanh")(pi)
+    #
+    #action_probs = k.Dense(action_dim, name="Out_probs",
+    #                     activation='softmax')(pi)
+    model = Model(inputs=state, outputs=[mlp2, value_pred])
+
+    model.summary()
+
+    return model
+
+#if __name__ == '__main__':
+#model = ctach_model((136,), 55)
+#print(model([np.random.rand(1, 136)]))
+# model.compile(optimizer='adam', loss='mse')
+# model.fit([np.random.rand(1, 136), np.random.rand(1, 136)], [np.random.rand(1, 50), np.random.rand(1, 1)], epochs=1)
--- PPO/main_test.py
+++ PPO/main_test.py
@@ -7,19 +7,6 @@ from keras import Model
 from tensorflow.python.framework.ops import enable_eager_execution
 enable_eager_execution()
 
-model_config = {
-    'custom_model': "keras_conv_lstm",
-    'custom_options': {
-        'fc_dim': 128,
-        'idx_emb_dim': 4,
-        'input_emb_vocab': 100,
-        'lstm_cell_size': 128,
-        'num_conv': 2,
-        'num_fc': 2,
-    },
-    'max_seq_len': 25,
-}
-
 env_config_wrapper = {
     'env_config_dict': {
         # ===== SCENARIO CLASS =====
@@ -102,61 +89,6 @@ env_config_wrapper = {
     }
 }
 
-env_config = {
-    # ===== SCENARIO CLASS =====
-    # Which Scenario class to use: the class's name in the Scenario Registry (foundation.scenarios).
-    # The environment object will be an instance of the Scenario class.
-    'scenario_name': 'layout_from_file/simple_wood_and_stone',
-    
-    # ===== COMPONENTS =====
-    # Which components to use (specified as list of ("component_name", {component_kwargs}) tuples).
-    #   "component_name" refers to the Component class's name in the Component Registry (foundation.components)
-    #   {component_kwargs} is a dictionary of kwargs passed to the Component class
-    # The order in which components reset, step, and generate obs follows their listed order below.
-    'components': [
-        # (1) Building houses
-        ('Build', {'skill_dist': "pareto", 'payment_max_skill_multiplier': 3}),
-        # (2) Trading collectible resources
-        ('ContinuousDoubleAuction', {'max_num_orders': 5}),
-        # (3) Movement and resource collection
-        ('Gather', {}),
-    ],
-    
-    # ===== SCENARIO CLASS ARGUMENTS =====
-    # (optional) kwargs that are added by the Scenario class (i.e. not defined in BaseEnvironment)
-    'env_layout_file': 'quadrant_25x25_20each_30clump.txt',
-    'starting_agent_coin': 10,
-    'fixed_four_skill_and_loc': True,
-    
-    # ===== STANDARD ARGUMENTS ======
-    # kwargs that are used by every Scenario class (i.e. defined in BaseEnvironment)
-    'n_agents': 4,          # Number of non-planner agents (must be > 1)
-    'world_size': [25, 25], # [Height, Width] of the env world
-    'episode_length': 1000, # Number of timesteps per episode
-    
-    # In multi-action-mode, the policy selects an action for each action subspace (defined in component code).
-    # Otherwise, the policy selects only 1 action.
-    'multi_action_mode_agents': False,
-    'multi_action_mode_planner': True,
-    
-    # When flattening observations, concatenate scalar & vector observations before output.
-    # Otherwise, return observations with minimal processing.
-    'flatten_observations': True,
-    # When Flattening masks, concatenate each action subspace mask into a single array.
-    # Note: flatten_masks = True is required for masking action logits in the code below.
-    'flatten_masks': True,
-}
-
-# def to_shape(a, shape):
-#     y_, x_ = shape
-#     y, x = a.shape
-#     y_pad = (y_ - y)
-#     x_pad = (x_ - x)
-#     return np.pad(a, ((y_pad // 2, y_pad // 2 + y_pad % 2),
-#                       (x_pad // 2, x_pad // 2 + x_pad % 2)),
-#                   mode='constant')
-
-
 def dict_to_tensor_dict(a_dict: dict):
     """
     pass a single agent obs, returns it's tensor_dict
@@ -169,13 +101,15 @@ def dict_to_tensor_dict(a_dict: dict):
     return tensor_dict 
 
 if __name__ == '__main__':
+    epochs = 100
+
     env = foundation.make_env_instance(**env_config_wrapper['env_config_dict']) #Edited
     #env = foundation.make_env_instance(**env_config) # Original
     obs = env.reset()
 
     model: Model = get_model([16, 32], 3)
 
-    for i in range(100):
+    for i in range(epochs):
         actions_dict = {}
         for agent_id in obs.keys():
             if agent_id != 'p':
