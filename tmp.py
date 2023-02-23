6b63579ad0b0073b4f10e27b92dc439c37a07485
sa1g <ettoreitaut4.1@gmail.com>
2023-02-23 12:16:05 +0100

    DEV: implemented LSTM model

--- tmp.py
+++ tmp.py
@@ -1,36 +1,14 @@
 from trainer.environment import get_environment
+from trainer.models import LSTMModel
 import ray
 from copy import deepcopy
 
 if __name__ == "__main__":
-    ray.init()
+    
     env = get_environment("cpu")
-    # obs = env.reset()
-
-    # for key in obs.keys():
-    #     for object in obs[key].keys():
-    #         print(f"{key} {object} {obs[key][object].shape}")
-
-    # Define the Counter actor.
-    @ray.remote
-    class Worker:
-        def __init__(self, env):
-            self.env = deepcopy(env)
-            self.obs = None
-
-        def reset(self):
-            self.obs = self.env.reset()
-
-        def get_obs(self,):
-            return self.obs
-
-    # Create a Counter actor.
-    c = Worker.remote(env)
-
-    # Submit calls to the actor. These calls run asynchronously but in
-    # submission order on the remote actor process.
-    c.reset.remote()
-
-    # Retrieve final actor state.
-    print(ray.get(c.get_obs.remote()))
-    # -> 10
+    
+    obs = env.reset()
+    model_planner = LSTMModel(env.observation_space_pl, 22, 'cpu')
+    action, logprob = model_planner.act(obs['p'])
+    model_planner.evaluate(obs['p'], action)
+    # model_agents = LSTMModel(env.observation_space, 50, 'cpu')
