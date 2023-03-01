774676d0dc041fff24d3098f2d39aa8242179063
sa1g <ettoreitaut4.1@gmail.com>
2023-03-01 14:23:44 +0100

    DEV: add save/load models

--- tmp.py
+++
@@ -1,9 +0,0 @@
-from trainer.environment import get_environment
-
-if __name__ == "__main__":
-    env = get_environment("cpu")
-    obs = env.reset()
-
-    for key in obs.keys():
-        for object in obs[key].keys():
-            print(f"{key} {object} {obs[key][object].shape}")
