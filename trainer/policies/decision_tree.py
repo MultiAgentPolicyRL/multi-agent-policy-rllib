1657b415e1e79bb5ff510e1cb0c093d3ceb71cba
bonom <andrea.bonomi489@gmail.com>
2023-03-15 09:55:34 +0100

    DEV: add decision tree support

--- trainer/policies/decision_tree.py
+++ trainer/policies/decision_tree.py
@@ -1,27 +1,5 @@
+import abc
 import numpy as np
-
-from ai_economist_ppo_dt.torch.models.actor_critic import LSTMModel
-
-
-def string_to_dict(x):
-    """
-    This function splits a string into a dict.
-    The string must be in the format: key0-value0#key1-value1#...#keyn-valuen
-    """
-    result = {}
-    items = x.split("#")
-
-    for i in items:
-        key, value = i.split("-")
-        try:
-            result[key] = int(value)
-        except:
-            try:
-                result[key] = float(value)
-            except:
-                result[key] = value
-
-    return result
 
 
 class DecisionTree:
@@ -29,10 +7,10 @@ class DecisionTree:
         self.current_reward = 0
         self.last_leaf = None
 
-    # @abc.abstractmethod
-    # def get_action(self, input):
-    #     pass
-
+    @abc.abstractmethod
+    def get_action(self, input):
+        pass
+    
     def set_reward(self, reward):
         self.current_reward = reward
 
@@ -44,41 +22,36 @@ class Leaf:
     def get_action(self):
         pass
 
-    def update(self):
+    def update(self, x):
         pass
 
 
 class QLearningLeaf(Leaf):
-    def __init__(self, rl_model: LSTMModel):  # , learning_rate, discount_factor):
-        self.rl_model = rl_model
-
-        # self.learning_rate = learning_rate
-        # self.discount_factor = discount_factor
+    def __init__(self, n_actions, learning_rate, discount_factor):
+        self.n_actions = n_actions
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
         self.parent = None
-
-        n_actions = rl_model.output_size
         self.iteration = [1] * n_actions
 
         self.q = np.zeros(n_actions, dtype=np.float32)
         self.last_action = 0
-        self.last_logits = None
-        self.last_value = 0
 
-    def get_action(self, x):
-        # action = np.argmax(self.q)
+    def get_action(self):
+        action = np.argmax(self.q)
+        self.last_action = action
+        return action
 
-        # This is already done in parent
-        # self.last_action = action
-        return self.rl_model(x)
-
-    def update(self, qprime):
+    def update(self, reward, qprime):
         if self.last_action is not None:
-            for action in self.last_action:
-                self.q[int(action.item())] += qprime
+            lr = self.learning_rate if not callable(self.learning_rate) else self.learning_rate(self.iteration[self.last_action])
+            if lr == "auto":
+                lr = 1/self.iteration[self.last_action]
+            self.q[self.last_action] += lr * (
+                        reward + self.discount_factor * qprime - self.q[self.last_action])
 
     def next_iteration(self):
-        for action in self.last_action:
-            self.iteration[int(action.item())] += 1
+        self.iteration[self.last_action] += 1
 
     def __repr__(self):
         return ", ".join(["{:.2f}".format(k) for k in self.q])
@@ -88,23 +61,33 @@ class QLearningLeaf(Leaf):
 
 
 class EpsGreedyLeaf(QLearningLeaf):
-    def __init__(self, rl_model: LSTMModel):
-        super().__init__(rl_model=rl_model)
+    def __init__(self, n_actions, learning_rate, discount_factor, epsilon):
+        super().__init__(n_actions, learning_rate, discount_factor)
+        self.epsilon = epsilon
 
-    def get_action(self, x):
-        action, policy, value = super().get_action(x)
+    def get_action(self):
+        if np.random.uniform() < self.epsilon:
+            action = np.random.randint(self.n_actions)
+        else:
+            # Get the argmax. If there are equal values, choose randomly between them
+            best = [None]
+            max_ = -float("inf")
+            
+            for i, v in enumerate(self.q):
+                if v > max_:
+                    max_ = v
+                    best = [i]
+                elif v == max_:
+                    best.append(i)
+
+            action = np.random.choice(best)
 
         self.last_action = action
-        self.last_logits = policy
-        self.last_value = value
-
         self.next_iteration()
-
-        return action, policy, value
-
+        return action
 
 class RandomlyInitializedEpsGreedyLeaf(EpsGreedyLeaf):
-    def __init__(self, rl_model: LSTMModel):
+    def __init__(self, n_actions, learning_rate, discount_factor, epsilon, low=-100, up=100):
         """
         Initialize the leaf.
         Params:
@@ -115,69 +98,73 @@ class RandomlyInitializedEpsGreedyLeaf(EpsGreedyLeaf):
             - low: lower bound for the initialization
             - up: upper bound for the initialization
         """
-        super(RandomlyInitializedEpsGreedyLeaf, self).__init__(rl_model=rl_model)
-
-
-class CLeaf(RandomlyInitializedEpsGreedyLeaf):
-    def __init__(self, rl_model, *args, **kwargs):
-        super(CLeaf, self).__init__(rl_model=rl_model)
+        super(RandomlyInitializedEpsGreedyLeaf, self).__init__(n_actions, learning_rate, discount_factor, epsilon)
+        self.q = np.random.uniform(low, up, n_actions)
 
 
 class PythonDT(DecisionTree):
-    def __init__(self, phenotype, rl_model: LSTMModel):
+    def __init__(self, phenotype, leaf, planner=False):
         super(PythonDT, self).__init__()
         self.program = phenotype
         self.leaves = {}
+        self.planner = planner
         n_leaves = 0
 
         while "_leaf" in self.program:
-            new_leaf = CLeaf(rl_model=rl_model)
-            leaf_name = "leaf_{}".format(n_leaves)
-            self.leaves[leaf_name] = new_leaf
+            if planner:
+                for i in range(7):
+                    new_leaf = leaf(22)
+                    leaf_name = "leaf_{}_{}".format(n_leaves, i)
+                    self.leaves[leaf_name] = new_leaf
 
-            self.program = self.program.replace(
-                "_leaf", "'{}.get_action()'".format(leaf_name), 1
-            )
-            self.program = self.program.replace("_leaf", "{}".format(leaf_name), 1)
+                    self.program = self.program.replace("_leaf", "'{}.get_action()'".format(leaf_name), 1)
+                    self.program = self.program.replace("_leaf", "{}".format(leaf_name), 1)
+            else:
+                new_leaf = leaf()
+                leaf_name = "leaf_{}".format(n_leaves)
+                self.leaves[leaf_name] = new_leaf
 
-            n_leaves += 1
+                self.program = self.program.replace("_leaf", "'{}.get_action()'".format(leaf_name), 1)
+                self.program = self.program.replace("_leaf", "{}".format(leaf_name), 1)
+
+                n_leaves += 1
         self.exec_ = compile(self.program, "<string>", "exec", optimize=2)
-
-    def get_action(self, x: dict):
+    
+    def get_action(self, input):
         if len(self.program) == 0:
             return None
         variables = {}  # {"out": None, "leaf": None}
-
-        _in = x.get("flat", None).squeeze()
-        for idx, value in enumerate(_in):
-            variables["_in_{}".format(idx)] = value
+        for idx, i in enumerate(input):
+            variables["_in_{}".format(idx)] = i
         variables.update(self.leaves)
+
+        if self.planner:
+            actions = []
+            # Subdivide the space in 7 parts and get the action for each one
+            for i in range(7):
+                variables["_in_0"] = i
+                exec(self.exec_, variables)
+                current_leaf = self.leaves[variables["leaf"]]
+
+                current_q_value = max(current_leaf.q)
+                if self.last_leaf is not None:
+                    self.last_leaf.update(self.current_reward, current_q_value)
+                self.last_leaf = current_leaf
+
+                actions.append(current_leaf.get_action())
+            
+            return actions
 
         exec(self.exec_, variables)
 
-        current_leaf: CLeaf = self.leaves[variables["leaf"]]
-
-        # current_q_value = max(current_leaf.q) # Here goes the value predicted from the model
-        # if self.last_leaf is not None:
-        #     self.last_leaf.update(self.current_reward, current_q_value)
-
-        self.last_leaf = current_leaf
-
-        action, logit, value = current_leaf.get_action(x)
-
-        current_q_value = value
+        current_leaf = self.leaves[variables["leaf"]]
+        current_q_value = max(current_leaf.q)
         if self.last_leaf is not None:
-            self.last_leaf.update(current_q_value)
-
-        if action is None:
-            print("[DECISION TREE] action is None")
-        if logit is None:
-            print("[DECISION TREE] logit is None")
-        if value is None:
-            print("[DECISION TREE] value is None")
-
-        return action, logit, value
-
+            self.last_leaf.update(self.current_reward, current_q_value)
+        self.last_leaf = current_leaf 
+        
+        return current_leaf.get_action()
+    
     def __call__(self, x):
         return self.get_action(x)
 
